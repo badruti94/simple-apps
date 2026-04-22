c89e8ac25756df3a36199fb4c2d0f950cb1eb27d
Badru <badruakfm@gmail.com>
2026-04-22 09:30:35 +0700

    update

--- otp-generator/app.js
+++ otp-generator/app.js
@@ -3,8 +3,13 @@ import express from 'express';
 const app = express();
 
 app.get('/api/generate-otp', (req, res) => {
-    const otp = Math.floor(100000 + Math.random() * 900000);
-    res.json({ otp: otp.toString() });
+    try {
+        const otp = Math.floor(100000 + Math.random() * 900000);
+        res.json({ otp: otp.toString() });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg: 'error'});
+    }
 });
 
 const port = Number(process.env.PORT ?? 3000);
